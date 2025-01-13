92719500b546a15e3b39ecf28e6aa8b3906531e9
Travis Smith <travis.smith@shiftdsm.com>
2025-01-12 23:55:39 -0600

    Deploy to GitHub Pages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -21,6 +22,11 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
     devServer: {
         static: './public',
     },
